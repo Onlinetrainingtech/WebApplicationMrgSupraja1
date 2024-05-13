8b83d030f0d2c0f14518cd2580e2613ddae0df7c
azartechnet <azartechnet@gmail.com>
2024-05-13 13:03:03 +0530

    updated

---
+++ ReactJs/suprajaui/src/App.js
@@ -0,0 +1,9 @@
+const Sample=()=>{
+    return(
+        <div>
+            <h1>This is App Component</h1>
+        </div>
+    )
+}
+
+export default Sample;
--- ReactJs/suprajaui/src/index.js
+++ ReactJs/suprajaui/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 
 /*function sample()
@@ -109,7 +109,7 @@ r1.render(myelem())*/
 
 //Class Component
 
-class Sample extends React.Component
+/*class Sample extends React.Component
 {
   render(){
     return(
@@ -120,4 +120,160 @@ class Sample extends React.Component
   }
 }
 const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<Sample/>)
+r1.render(<Sample/>)*/
+
+//Function Component with arguments
+
+/*function Sample(props)
+{
+  return(
+    <>
+      <h1>Welcome to the function arguments</h1>
+      <p>{props.name}</p>
+      <p>{props.age}</p>
+    </>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample name={"John"} age={34}/>)*/
+
+//Component in Component
+/*function Sample()
+{
+  return(
+    <h1>This is FirstComponent`</h1>
+  )
+}
+function Sample2()
+{
+  return(
+    <>
+    <h2>This is Second Component</h2>
+    <Sample />
+    </>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample2 />)*/
+
+//Component in file or importing and exporting the file
+
+/*import Sample from './App'
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample />)*/
+
+//Component Constructor
+
+/*class Sample extends React.Component
+{
+  constructor(){
+    super();
+    this.state = {name:"John"}
+  }
+  render()
+  {
+    return(
+      <h1>I am Sample::{this.state.name}</h1>
+    )
+  }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>)*/
+
+//React State
+
+/*class Sample extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      empid:1001,
+      ename:"mohamed",
+      salary:5000
+    }
+  }
+  render()
+  {
+    return(
+      <div>
+        <h1>My EmployeeId::{this.state.empid}</h1>
+        <h2>MyEmployeeName::{this.state.ename}</h2>
+        <h3>MySalary::{this.state.salary}</h3>
+      </div>
+    )
+  }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample />)*/
+
+//Changing the state object
+
+/*class Sample extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      emp:{
+        id:1001,
+        name:"azar",
+        city:"karur"
+      },
+      showData:false
+    };
+  }
+  showData()
+  {
+    this.setState({showData:true})
+  }
+  hideData()
+  {
+    this.setState({showData:false})
+  }
+  render()
+  {
+    let data;
+    if (this.state.showData==true)
+    {
+       data=<div><b>City:</b>{this.state.emp.city}
+       <button onClick={this.hideData.bind(this)}>Hide Data</button></div>
+
+    }
+    else
+    {
+      data=<button onClick={this.showData.bind(this)}>ShowData</button>
+    }
+    return(
+      <div className='App'>
+        <h1>EmployeeDetails</h1>
+        {data}
+      </div>
+    )
+  }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample />);*/
+
+//Using Constructor
+
+class Header extends Component
+{
+  text="Welcome"
+  constructor(props)
+  {
+    super(props);
+    this.state={f1:'red'}
+  }
+  render()
+  {
+    return(
+      <h3 style={{color:this.state.f1}}>{this.props.text}</h3>
+    )
+  }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Header text="Helloworld"/>)
+
+//React Event
